850115af1f38213c8417161bc78b8f3ac3a6b775
ExclNyx <exclzx20@gmail.com>
2025-10-20 06:44:42 +0700

    coba lagi

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Codelab1 Pemrograma Lanjut MOdule 3n!");
+        System.out.println("Codelab1 Pemrograma Lanjut Module 3!");
     }
 }
