f34977904af41ec835153eccd9ac086aaaa169c3
ExclNyx <exclzx20@gmail.com>
2025-10-20 08:13:03 +0700

    Program kalkulator

--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Codelab1 Pemrograman Lanjut Module 3, NEWS!");
+        Scanner input = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("Kalkulator");
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 == 0) {
+                    System.out.println("Pembagian dengan nol tidak diperbolehkan!");
+                    return;
+                }
+                hasil = angka1 / angka2;
+                break;
+            default:
+                System.out.println("Operator tidak valid!");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
     }
 }
