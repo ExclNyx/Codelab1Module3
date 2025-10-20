55742bb6a5599b7e21086f3ef01725b028f8d802
ExclNyx <exclzx20@gmail.com>
2025-10-20 07:24:53 +0700

    Penambahan NEWS

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Codelab1 Pemrograma Lanjut Module 3!");
+        System.out.println("Codelab1 Pemrograman Lanjut Module 3, NEWS!");
     }
 }
